87f9681fb440c406a681deeb25920e2a33352dda
Kennnedu <r.kenda94@gmail.com>
2019-04-19 10:36:35 +0300

    add purecss and style loader packages; create Layout component with navbar; rename folder javascripts to frontend; relace bundle.js to public forlder

---
+++ frontend/RatelApp.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="ratel-app">
+		<Layout>
+			Hello world!
+		</Layout>
+	</div>
+</template>
+
+<script>
+	import Layout from './Layout.vue'
+
+	export default {
+		components: {
+			Layout
+		}
+	}
+</script>
+
+<style lang="scss">
+</style>
