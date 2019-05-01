3f7f7fe07bd32a725e8336ed9d360761f79d7439
Kennnedu <r.kenda94@gmail.com>
2019-05-01 22:11:19 +0300

    split uploading component into two components HtmlStatemensForm and RecordForm

--- frontend/components/Uploading.vue
+++ frontend/components/Uploading.vue
@@ -5,79 +5,18 @@
       <h2>The page allows you to upload new statements</h2>
     </div>
     <div class="content">
-      <form class="pure-form pure-form-stacked">
-        <fieldset>
-          <label for="statements-html">Statements HTML</label>
-          <textarea id="statements-html"
-                    placeholder="Past your statements html table"
-                    v-bind:disabled="isDisabledTextArea"
-                    v-model="htmlTable"/>
-          <span class="pure-form-message pure-form-message-error" v-for="(errorMessage, index) in errors" v-if="errorMessage !== ''">
-            {{ nubmerOfError === 1 ? errorMessage : `${index + 1} line - ${errorMessage}` }}
-          </span>
-        </fieldset>
-        <input type="submit"
-               class="pure-button pure-button-primary"
-               v-bind:value="saveButtonName"
-               v-on:click="saveStatements">
-      </form>
+      <RecordForm/>
+      <HtmlStatementsForm/>
     </div>
   </div>
 </template>
 <script>
   import axios from 'axios'
+  import RecordForm from './uploading/RecordForm.vue'
+  import HtmlStatementsForm from './uploading/HtmlStatementsForm.vue'
 
   export default {
-    data: function(){
-      return {
-        htmlTable: '',
-        errors: [],
-        saveButtonName: 'Upload'
-      }
-    },
-    computed: {
-      isDisabledTextArea: function() { return this.saveButtonName !== 'Upload' },
-      nubmerOfError: function() { return this.errors.length },
-    },
-    methods: {
-      saveStatements: function(){
-        let _this = this;
-        _this.errors = [];
-
-        _this.saveButtonName = 'Parsing...';
-
-        axios.post('/records/bulk/parse', { html_table: _this.htmlTable })
-          .then(function(response){
-            _this.saveButtonName = 'Validating...';
-            console.log(response);
-
-            axios.post('/records/bulk/validate', { records: response.data.records })
-              .then(function(response){
-                console.log(response);
-                _this.saveButtonName = 'Saving...';
-
-                axios.post('/records/bulk', { records: response.data.records })
-                  .then(response => {
-                    console.log(response);
-                    _this.saveButtonName = 'Upload';
-                    _this.htmlTable = '';
-                  })
-                  .catch(error => _this.errorHandler(error.response));
-              })
-              .catch(error => _this.errorHandler(error.response));
-          })
-          .catch(error => _this.errorHandler(error.response));
-      },
-      errorHandler: function(response){
-        console.log(response);
-        this.saveButtonName = 'Upload';
-        if(Array.isArray(response.data.message)) {
-          this.errors = [...this.errors, ...response.data.message];
-        } else {
-          this.errors.push(response.data.message)
-        }
-      }
-    }
+    components: { HtmlStatementsForm, RecordForm }
   }
 </script>
 <style lang="css" scoped>
---
+++ frontend/components/uploading/RecordForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="pure-form pure-form-stacked">
+  </form>
+</template>
+<script>
+  import axios from 'axios'
+
+  export default {
+    data: function(){
+      return {
+        record: {
+          name: '',
+          card: '',
+          amount: 0.0,
+          rest: 0.0,
+          performedAt: new Date()
+        },
+        errors: [],
+        saveButtonName: 'Saving'
+      }
+    }
+  }
+</script>
+<style lang="css" scoped>
+  .pure-form-message.pure-form-message-error {
+    color: red;
+  }
+</style>
