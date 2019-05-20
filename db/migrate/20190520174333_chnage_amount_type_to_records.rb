5c26e0cedd6fdb30be9704c730af9ecdd80cb653
Kennnedu <r.kenda94@gmail.com>
2019-05-20 20:58:21 +0300

    make amount type as decimal

---
+++ db/migrate/20190520174333_chnage_amount_type_to_records.rb
@@ -0,0 +1,9 @@
+class ChnageAmountTypeToRecords < ActiveRecord::Migration[5.2]
+  def up
+    change_column :records, :amount, :decimal 
+  end
+
+  def down
+    change_column :records, :amount, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_06_104239) do
+ActiveRecord::Schema.define(version: 2019_05_20_174333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_04_06_104239) do
   create_table "records", force: :cascade do |t|
     t.string "name"
     t.string "card"
-    t.float "amount"
+    t.decimal "amount"
     t.float "rest"
     t.datetime "performed_at"
     t.datetime "created_at", null: false
