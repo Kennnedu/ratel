d17ba5946dc81a0c150de1792a64ead10077109b
Kennnedu <r.kenda94@gmail.com>
2019-06-02 16:05:55 +0300

    add migration to add user_id and indexing name and card fields to records

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_112026) do
+ActiveRecord::Schema.define(version: 2019_06_02_130015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2019_06_02_112026) do
     t.datetime "performed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["card"], name: "index_records_on_card"
+    t.index ["name"], name: "index_records_on_name"
   end
 
   create_table "users", force: :cascade do |t|
