2b4e339ef1de893316f194ac252bdf0f01aa2953
Kennnedu <r.kenda94@gmail.com>
2019-06-30 16:29:00 +0300

    replace totalSum from statements comp to Navigation add style to display total sum

--- frontend/components/Navigation.vue
+++ frontend/components/Navigation.vue
@@ -11,13 +11,21 @@
       <div class="pure-menu">
         <a class="pure-menu-heading" href="#">Ratel</a>
         <ul class="pure-menu-list">
-          <li class="pure-menu-item"
-              v-for="page in navigationMenu"
-              v-bind:class="{'menu-item-divided pure-menu-selected': currentPage === page}">
+          <li
+            class="pure-menu-item"
+            v-for="page in navigationMenuPages"
+            v-bind:class="{'pure-menu-selected': currentPage === page,
+                           'menu-item-divided': navigationMenuPages[0] === page}">
             <a href="#"
                class="pure-menu-link"
                v-on:click="$emit('navigateTo', page)">
               {{page}}
+              <span
+                class="total-sum"
+                v-bind:class="{'positive': parseFloat(totalSum) > 0, 'negative': parseFloat(totalSum) < 0}"
+                v-if="page === 'Records'">
+                {{totalSum}}
+              </span>
             </a>
           </li>
         </ul>
@@ -31,17 +39,34 @@
 
 <script>
   import '../stylesheets/navigation.css'
+  import { mapState } from 'vuex'
 
   export default {
     props: ['currentPage'],
+
     data: function(){
       return {
         active: false,
-        navigationMenu: ['Statements', 'Uploading', 'Dashboard']
+        navigationMenuPages: ['Uploading', 'Dashboard', 'Records'],
       }
+    },
+
+    computed: {
+      ...mapState(['totalSum'])
     }
   }
 </script>
 
 <style lang="css">
+  span.total-sum {
+    font-size: 12px;
+  }
+
+  span.total-sum.positive {
+    color: #a0f8a0
+  }
+
+  span.total-sum.negative {
+    color: #ff5b5b
+  }
 </style>
