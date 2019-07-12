de593e6dc2a3321e3a1c00252ed5a778a424f20a
Kennnedu <r.kenda94@gmail.com>
2019-07-13 00:52:10 +0300

    add chart to dashboard

--- frontend/components/Dashboard.vue
+++ frontend/components/Dashboard.vue
@@ -1,22 +1,8 @@
 <template>
   <div class="content">
     <div class="pure-g">
-      <div class="pure-u-1">
-        <h3>Card balances</h3>
-        <table class="pure-table">
-          <thead>
-            <tr>
-              <th>Card</th>
-              <th>Amount Sum</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="line in dashboardData.cardsData">
-              <td>{{ line[0] }}</td>
-              <td>{{ `${line[1]} BYN` }}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="pure-u-1 pure-u-md-1-3">
+        <canvas id="cards-data" width="300" height="300" />
       </div>
       <div class="pure-u-1">
         <h3>Replenishments sums</h3>
@@ -58,10 +44,64 @@
 </template>
 <script>
   import { mapState } from 'vuex';
+  import Chart from 'chart.js'
 
   export default {
     computed: {
       ...mapState(['dashboardData'])
+    },
+
+    watch: {
+      dashboardData: {
+        handler() {
+          this.cardsChart.destroy()
+          this.cardsChart = this.buildChart();
+        }
+      },
+
+      deep: true
+    },
+
+    mounted() {
+      this.cardsChart = this.buildChart()
+    },
+
+    methods: {
+      buildChart() {
+        const _this = this;
+
+        return new Chart(document.getElementById('cards-data'), {
+          type: 'doughnut',
+          data: {
+            datasets: [{
+              data: _this.dashboardData.cardsData.map(el => el[1]),
+              label: 'Cards',
+              backgroundColor: [
+                "rgb(54, 162, 235)",
+                "rgb(255, 159, 64)",
+                "rgb(255, 205, 86)",
+                "rgb(75, 192, 192)",
+                "rgb(54, 162, 235)"
+              ].sort(() => Math.random() - 0.5)
+            }],
+            labels: _this.dashboardData.cardsData.map(el => el[0])
+          },
+          options: {
+            responsive: true,
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Card\'s Balances'
+            },
+            animation: {
+              animateScale: true,
+              animateRotate: true
+            }
+          }
+        })
+      }
     }
   }
 </script>
