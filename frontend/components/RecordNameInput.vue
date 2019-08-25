4c55393aa1047f4105051275f0940720e393bdd3
Kennnedu <sir.kenda@yandex.ru>
2019-08-25 19:22:44 +0300

    added RecordNameInput comp

---
+++ frontend/components/RecordNameInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <!-- <input
+      type="text"
+      placeholder="Press space to add new tag"
+      list="suggested-tags"
+      v-model="tagName"
+      v-on:keyup.space="addRecordsTag" />
+ -->
+    <label for="record-name">Operation</label>
+    <input
+      type="text"
+      id="record-name"
+      placeholder="Enter operation"
+      v-model.trim="currentRecord.name"
+      required />
+
+    <datalist id="suggested-record-names">
+      <option v-for="recName in suggestedRecordNames" v-bind:key="recName" v-bind:value="recName" />
+    </datalist>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  props: ['recordName'],
+
+  data: function(){
+    return {
+      suggestedRecordNames: []
+    }
+  },
+
+  watch: {
+
+  }
+}
+</script>
+<style lang="css" scoped>
+  .tags span {
+    font-size: 13px;
+    background-color: #ababa9;
+    padding: 3px;
+    color: white;
+    border-radius: 6px;
+    margin-right: 3px;
+  }
+
+  .tags span svg {
+    margin-left: 2px;
+  }
+</style>
