9c7f37bc0bc9c304ab5aec7181b0ebc1c9efe9b2
Kennnedu <r.kenda94@gmail.com>
2020-08-21 16:49:37 +0300

    added validation to tag and records_tag

--- app/models/records_tag.rb
+++ app/models/records_tag.rb
@@ -3,4 +3,6 @@
 class RecordsTag < ActiveRecord::Base
   belongs_to :record
   belongs_to :tag
+
+  validates :record_id, uniqueness: { scope: :tag_id }
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,11 +5,18 @@ class Tag < ActiveRecord::Base
   has_many :records_tags, dependent: :destroy
   has_many :records, through: :records_tags
 
-  # validates :name, :user_id, uniqueness: true, presence: true
+  validates :name, uniqueness: { scope: :user_id }
+  validates :name, presence: true
 
   scope :join_record_query, lambda { |query|
     joins('left join records_tags on records_tags.tag_id = tags.id')
       .joins("left join (#{query}) records on records_tags.record_id = records.id")
       .group('tags.id')
   }
+
+  before_save :prepare_name
+
+  def prepare_name
+    self.name = name.strip.downcase
+  end
 end
