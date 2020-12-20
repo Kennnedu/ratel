f9c2e781cca2fa6e85ce90e763e506670f2441de
Kennnedu <r.kenda94@gmail.com>
2020-12-20 14:36:23 +0300

    added gmail_connection db table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  has_one :gmail_connection, dependent: :destroy
   has_many :records, dependent: :destroy
   has_many :cards, dependent: :destroy
   has_many :tags, dependent: :destroy
