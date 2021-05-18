1411d28d2b8400d568001e54779255ca912ab1b6
Kennnedu <r.kenda94@gmail.com>
2021-05-18 16:08:38 +0300

    added logic to update record

---
+++ lib/services/update_record.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Services
+  class UpdateRecord
+    def process(record, params)
+      @record = record
+      @tags = params.delete('tags')
+      assign_tags
+      @record.assign_attributes params
+      @record.save
+    end
+
+    private
+
+    def assign_tags
+      return unless @tags
+      tags_attr = []
+      all_tags = @record.user.tags.map(&:name)
+      record_tags = @record.tags.map(&:name)
+
+      create_and_set = @tags - all_tags
+      set_tags = (@tags & all_tags) - record_tags
+      remove_tags = record_tags - @tags
+
+      create_and_set.each do |tag|
+        tag_obj = @record.user.tags.create name: tag
+        tags_attr << { tag_id: tag_obj.id }
+      end
+
+      set_tags.each do |tag|
+        tag_obj = @record.user.tags.find_by_name tag
+        tags_attr << { tag_id: tag_obj.id }
+      end
+
+      remove_tags.each do |tag|
+        tag_obj = @record.user.tags.find_by_name tag
+        rec_tag = @record.records_tags.find_by_tag_id tag_obj.id
+        tags_attr << { id: rec_tag.id, _destroy: 1 }
+      end
+
+      @record.records_tags_attributes = tags_attr
+    end
+  end
+end
