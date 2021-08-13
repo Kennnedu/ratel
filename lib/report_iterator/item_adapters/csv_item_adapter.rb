1fddbd023f61c06ce381bdb3f6f92e0baa482a2c
Kennnedu <r.kenda94@gmail.com>
2021-08-13 11:49:35 +0300

    added csv iterator

---
+++ lib/report_iterator/item_adapters/csv_item_adapter.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module ReportIterator
+  module ItemAdapters
+    class CsvItemAdapter
+      attr_accessor :item, :card
+
+      def as_json
+        { 'name' => name, 'amount' => amount, 'performed_at' => performed_at }.tap do |item_hash|
+          item_hash['card'] = card if card
+        end
+      end
+
+      protected
+
+      def name
+        @item['note'].gsub(/\s+/, ' ').strip
+      end
+
+      def amount
+        @item['amount_account_currency'].to_f
+      end
+
+      def performed_at
+        @last_peformed_at = DateTime.parse(item['date']).to_s
+      rescue StandardError
+        @last_peformed_at ||= Time.now
+      end
+    end
+  end
+end
---
+++ lib/report_iterator/props/csv_props.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+module ReportIterator
+  module Props
+    class CsvProps
+      PARSE_OPTS = {
+        headers: %w[date note amount_account_currency account_currency_code amount_transaction_currency
+                    transaction_currency_code],
+        col_sep: ';',
+        encoding: 'windows-1251:utf-8',
+        quote_char: '|'
+      }.freeze
+
+      include Import['report_iterator.item_adapters.csv_item_adapter']
+
+      attr_accessor :report
+
+      def collection
+        initial_query[3..-1]
+      end
+
+      def item_adapter
+        csv_item_adapter.card = card
+        csv_item_adapter
+      end
+
+      protected
+
+      def initial_query
+        CSV.readlines(report, PARSE_OPTS)
+      end
+
+      def card
+        /(?<=Account number:)(.*)(?= Available balance)/.match(initial_query[0]['date']).to_s
+      end
+    end
+  end
+end
